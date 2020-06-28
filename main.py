04c98c9e6305cc01d396bf49cc148b7faadf1fe8
aditiyadwiramadani <57509180+aditiyadwiramadani@users.noreply.github.com>
2020-06-28 08:13:24 +0000

    Menabkan file main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = {'a':  np.arange(50),
+        'c':  np.random.randint(0,50,50),
+        'd':  np.random.randn(50)}
+
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('Ini a')
+plt.ylabel('ini b')
+plt.show()
